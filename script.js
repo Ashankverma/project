4d6a785004d96d42c0b11166467022e6cca16611
Ashankverma <ashankver@gmail.com>
2025-02-06 13:51:04 +0530

    add java script

---
+++ script.js
@@ -0,0 +1,26 @@
+const slider = document.querySelectorAll(".slide")
+// console.log(slide);
+var counter = 0;
+
+slide.forEach(
+    ()
+)
+
+// const goPrev = () =>{
+//     counter--
+//     alert()
+//     slideImage()
+// }
+
+// const goNext = () =>{
+//     counter++
+//     slideImage()
+// }
+
+// const slideImage = () =>{
+//     slide.forEach(
+//         (row)=>{
+//             row.style.transform='translateX(-${counter * 100}%)'
+//         }
+//     )
+// }
